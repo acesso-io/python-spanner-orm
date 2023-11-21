a7fc02be6c781adc3d829c525304cb6cd2d0eb64
Andre Tachian <andre.tachian@unico.io>
2023-11-21 17:46:49 -0300

    feat(ccore-83): Update python version

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ from setuptools import setup
 
 setup(
     name="spanner-orm",
-    version="0.5.1",
+    version="0.5.3",
     description="Basic ORM for Spanner",
     maintainer="Aniruddha Maru",
     maintainer_email="aniruddhamaru@gmail.com",
     url="https://github.com/maroux/python-spanner-orm",
     packages=["spanner_orm", "spanner_orm.admin"],
     include_package_data=True,
-    python_requires="~=3.7",
+    python_requires="~=3.11",
     install_requires=["wheel", "google-cloud-spanner >= 1.6, <2.0.0dev"],
     tests_require=["absl-py"],
     extras_require={
