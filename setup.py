2d413f4f9ff7b399523d16e1a22710cffd6337ea
Rodrigo Goulart <rodrigo.goulart@unico.io>
2024-10-08 21:10:31 -0300

    chore: update dependencies versions

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setup(
     packages=["spanner_orm", "spanner_orm.admin"],
     include_package_data=True,
     python_requires="~=3.7",
-    install_requires=["wheel", "google-cloud-spanner >= 1.6, <2.0.0dev"],
+    install_requires=["wheel", "google-cloud-spanner>=3.44.0,<4.0.0"],
     tests_require=["absl-py"],
     extras_require={
-        "tests": ["black==20.8b1"],
+        "tests": ["black==23.3.0"],
     },
     entry_points={"console_scripts": ["spanner-orm = spanner_orm.admin.scripts:main"]},
 )
